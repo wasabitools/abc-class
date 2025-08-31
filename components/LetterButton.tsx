d6e3500c8f25b1a5490df84e7021af3160d2cecb
natasafi <natasafrag@gmail.com>
2025-08-31 17:43:04 +0100

    improves styling

--- components/LetterButton.tsx
+++ components/LetterButton.tsx
@@ -26,7 +26,7 @@ export default function LetterButton({ image, letter, sound }: LetterButtonProps
             onClick={() => isClient && playSound()}
             className="w-20 h-20 rounded-full border-4 border-purple-400 flex items-center justify-center bg-white shadow-lg hover:shadow-xl transition-all transform hover:scale-105 ripple"
         >
-            <img src={image} alt={letter} />
+            <img src={image} alt={letter}  className="w-full h-full object-cover rounded-full" />
         </button>
 
     )
--- constants/alphabet.ts
+++ constants/alphabet.ts
@@ -1,6 +1,6 @@
 export const alphabetSoundPairs = [
-  { letter: 'A', image: '/letters/Aa.png', sound: '/sounds/a.mp3' },
-  { letter: 'B', image: '/letters/Bb.png', sound: '/sounds/b.mp3' },
-  { letter: 'C', image: '/letters/Cc.png', sound: '/sounds/c.mp3' },
-  { letter: 'D', image: '/letters/Dd.png', sound: '/sounds/d.mp3' },
+    { letter: 'A', image: '/letters/Aa.png', sound: '/sounds/a.mp3' },
+    { letter: 'B', image: '/letters/Bb.png', sound: '/sounds/b.mp3' },
+    { letter: 'C', image: '/letters/Cc.png', sound: '/sounds/c.mp3' },
+    { letter: 'D', image: '/letters/Dd.png', sound: '/sounds/d.mp3' },
 ]
